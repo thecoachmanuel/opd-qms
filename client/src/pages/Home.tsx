7eb698cb767c3d7e49bce9ace488496788fbf577
thecoachmanuel <manueloliver2908@gmail.com>
2026-01-06 00:25:22 +0100

    margin top added to hero section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -12,7 +12,7 @@ export const Home: React.FC = () => {
       <section className="bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 text-center">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block">{config.hero.title}</span>
+            <span className="block mt-[60px]">{config.hero.title}</span>
             <span className="block text-green-600">{config.hero.subtitle}</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
